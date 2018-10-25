b8a27347bf70fe4ff5d1f87694b19dda3a4fa24e
a021792876p <a021792876p@s09-pc19.ausias.lan>
2018-10-25 19:50:54 +0200

    Encriptacion de la pass de mier

--- src/main/java/net/carlosgrau/control/json.java
+++ src/main/java/net/carlosgrau/control/json.java
@@ -50,7 +50,7 @@ public class json extends HttpServlet {
                     strUser = request.getParameter("user");
                    
                         if (strOp.equalsIgnoreCase("login")) {
-                            if (strUser.equalsIgnoreCase("carlos") && strPass.equalsIgnoreCase("carlos")) {
+                            if (strUser.equalsIgnoreCase("carlos") && strPass.equalsIgnoreCase("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92")) {
                                 oSession.setAttribute("sesion", strUser);
                                 response.setStatus(200);
                                 status = "200";
